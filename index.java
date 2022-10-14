641ea4192478111d466b02809f3fe7ee0a42959d
ademuth <89747240+ademuth@users.noreply.github.com>
2022-10-13 22:22:34 -0500

    index fleshed out, asks for info and gives summary

--- index.java
+++ index.java
@@ -3,17 +3,27 @@
 
 <head>
   <meta charset="utf-8">
-  <title></title>
-  <meta name="author" content="">
-  <meta name="description" content="">
+  <title>Onboarding Project</title>
+  <meta name="author" content="Alex DeMuth">
+  <meta name="description" content="Sign-up">
   <meta name="viewport" content="width=device-width, initial-scale=1">
 
-  <link href="css/style.css" rel="stylesheet">
+  <link rel="stylesheet" media="screen" href="css/screen.css" />
 </head>
 
 <body>
-
-  <p>Hello, world!</p>
+  <header>
+    <h1>Chicago Hole-Digging Club</h1>
+    <p>Have you ever felt like there just aren&amp;t enough holes? Well we&amp;re here to change that! Sign up to dig holes around the city every week!</p>
+  </header>
+  <class="info">
+    <label>First Name</label>
+    <input type="text" name="firstname" placeholder="John">
+	<label>Last Name</label>
+	<input type="text" name="lastname" placeholder="Doe">
+	<label>Email:</label>
+	<input type="email" name="email" placeholder="xxxx@xxxx.com">
+  </class>
 
   <script src="js/script.js"></script>
 </body>
