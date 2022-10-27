9a3ccf10241617a9433c9ccac61da0016658dd9e
ademuth <89747240+ademuth@users.noreply.github.com>
2022-10-27 00:36:28 -0500

    created 4 const in javascript
    again, these will be used for form validation

---
+++ js/script.js
@@ -0,0 +1,4 @@
+const firstname = document.getElementById('firstname')
+const lastname = document.getElementById('lastname')
+const email = document.getElementById('email')
+const errorElement = document.getElementById('error')
