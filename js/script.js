2448993d8f568e720dd68365fbe2d7ab0e18ae00
ademuth <89747240+ademuth@users.noreply.github.com>
2022-10-27 00:55:27 -0500

    Added a different method of form validation
    the last attempt did not work so I am trying a function again

--- js/script.js
+++ js/script.js
@@ -3,13 +3,9 @@ const lastname = document.getElementById('lastname')
 const email = document.getElementById('email')
 const errorElement = document.getElementById('error')
 
-form.addEventListener('submit', (e) => {
-  let messages = []
-  if (firstname === '' || name.value == null) {
-	messages.push('Please enter your first name')
+function validateform(){  
+  if (firstname=='' || firstname==null){  
+    alert("Please enter your first name");  
+    return false;  
   }
-  if (messages.length > 0) {	
-	e.preventDefault()
-	errorElement.innerText = messages.join(', ')
-  }
-})
+}  
