59db4220e271020c3794dcdbb9786ed395e5d3c1
ademuth <89747240+ademuth@users.noreply.github.com>
2022-10-27 00:43:16 -0500

    added eventlistener to check if the first name field is actually filled out
    if not filled out, an error message is displayed

--- js/script.js
+++ js/script.js
@@ -2,3 +2,15 @@ const firstname = document.getElementById('firstname')
 const lastname = document.getElementById('lastname')
 const email = document.getElementById('email')
 const errorElement = document.getElementById('error')
+
+form.addEventListener('submit', (e) => {
+	let messages = []
+	if (firstname === '' || name.value == null) {
+	  messages.push('Please enter your first name')
+	}
+	
+	if (messages.length > 0) {	
+	  e.preventDefault()
+	  errorElement.innerText = messages.join(', ')
+	}
+})
