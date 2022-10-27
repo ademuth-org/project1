6794a371a5e47780b2125b77a3d6fef2501f659c
ademuth <89747240+ademuth@users.noreply.github.com>
2022-10-27 00:50:02 -0500

    fixed indentation for javascript

--- js/script.js
+++ js/script.js
@@ -4,13 +4,12 @@ const email = document.getElementById('email')
 const errorElement = document.getElementById('error')
 
 form.addEventListener('submit', (e) => {
-	let messages = []
-	if (firstname === '' || name.value == null) {
-	  messages.push('Please enter your first name')
-	}
-	
-	if (messages.length > 0) {	
-	  e.preventDefault()
-	  errorElement.innerText = messages.join(', ')
-	}
+  let messages = []
+  if (firstname === '' || name.value == null) {
+	messages.push('Please enter your first name')
+  }
+  if (messages.length > 0) {	
+	e.preventDefault()
+	errorElement.innerText = messages.join(', ')
+  }
 })
