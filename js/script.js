fa54c21b78884298307a7d8e46dcf0cd71f32dc2
ademuth <89747240+ademuth@users.noreply.github.com>
2022-10-27 01:52:56 -0500

    added some spacing

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ function validateform(){
   var firstname = document.getElementById('firstname').value;
   var lastname = document.getElementById('lastname').value;
   var email = document.getElementById('email').value;
-  if (firstname=="" || firstname==null){  
+  if (firstname == "" || firstname == null){  
     alert("Please enter your first name");  
   }
-}  
+}
